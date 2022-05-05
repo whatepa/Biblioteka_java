992b85f1e46a729e8f6f39344b72266e45c0912d
jgrzesik <jozef.grzesik239@gmail.com>
2022-05-05 14:19:59 +0200

    usnunięto niepotrzebne komentarze

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package biblioteka;
 
 
@@ -14,9 +10,6 @@ import java.util.Scanner;
  */
 public class Biblioteka {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         ekranStartowy();
--- src/biblioteka/Uzytkownik.java
+++ src/biblioteka/Uzytkownik.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package biblioteka;
 
 import java.io.FileNotFoundException;
@@ -11,9 +7,6 @@ import java.io.RandomAccessFile;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * @author Kuba
- */
 public class Uzytkownik {
     int Id_uzytkownika;
     String Imie;
